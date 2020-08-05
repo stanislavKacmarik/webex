099322e6e979d1760315ccb3eff67b24bd3e8ca2
kacmarik_stanislav <stanislav.kacmarik@proup.sk>
2020-08-05 18:09:58 +0200

    init commit (with vue js)

---
+++ resources/js/components/AppComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="navbar navbar-dark bg-primary">
+            <span class="navbar-brand mb-0 h1">Image galery - Webex</span>
+        </div>
+
+        <div class="container">
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'app',
+        components: {
+        }
+
+    }
+</script>
